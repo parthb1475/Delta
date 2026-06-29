b7a138d33f2c38ddb6616ac8918b345a343c113e
parthb1475 <parthbaraiya487@gmail.com>
2026-06-29 13:42:10 +0530

    againcomeback

---
+++ Day - 15/alert&prompts.js
@@ -0,0 +1,3 @@
+let firstName = prompt("enter your first name :");
+let lastName = prompt("enter your last name :");
+console.log("welcome", firstName, lastName, "!");
---
+++ Day - 15/truthy&falsy.js
@@ -0,0 +1,11 @@
+if(true) {
+    console.log("it has true value.")
+} else {
+    console.log("it has false value.")
+}
+
+if(false) {
+    console.log("it has true value.")
+} else {
+    console.log("it has false value.")
+}
