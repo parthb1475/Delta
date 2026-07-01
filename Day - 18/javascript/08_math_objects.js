17aaba5568c2d11a2c4f371a06adbb6568bb11a7
parthb1475 <parthbaraiya487@gmail.com>
2026-07-01 18:53:29 +0530

    objects

---
+++ Day - 18/javascript/08_math_objects.js
@@ -0,0 +1,19 @@
+Math.abs(-12);
+
+// output : 12
+
+Math.pow(2, 4);
+
+// output : 16
+
+Math.floor(5.5);
+
+// output : 5 (nearest smallest integer value)
+
+Math.ceil (6.5);
+
+// output : 7 (nearest Largest Integer Value)
+
+Math.random();
+
+// output : (random amount between 0 to 1 (not 0 or 1))
